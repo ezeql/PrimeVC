ad0e4888ae1b72c22d6366e4706f1b83d7b25fe9
Andrew Pahuru <andrew@onlinetouch.nl>
2013-02-07 18:08:41 +1300

    Setup prime-css mlib and munit files.

---
+++ prime-css/test/AllClassesTest.hx
@@ -0,0 +1,69 @@
+package ;
+
+import massive.munit.util.Timer;
+import massive.munit.Assert;
+import massive.munit.async.AsyncFactory;
+
+//import prime.tools.CSSParserMain;
+//import prime.tools.StyleSheet.tpl;
+//import prime.tools.CSSParser;
+import prime.bindable.collections.PriorityList;
+import prime.core.traits.IPrioritizable;
+import prime.gui.filters.BevelFilter;
+import prime.gui.filters.BitmapFilter;
+import prime.gui.filters.BitmapFilterType;
+import prime.gui.filters.BlurFilter;
+import prime.gui.filters.DropShadowFilter;
+import prime.gui.filters.GlowFilter;
+import prime.gui.filters.GradientBevelFilter;
+import prime.gui.filters.GradientGlowFilter;
+import prime.gui.filters.IBitmapFilter;
+import prime.tools.Manifest;
+import prime.tools.StopWatch;
+import prime.tools.generator.CodeGenerator;
+import prime.tools.generator.ICodeFormattable;
+import prime.tools.generator.ICodeGenerator;
+import prime.tools.generator.ICSSFormattable;
+import prime.tools.generator.Instance;
+import prime.tools.generator.InstanceType;
+import prime.tools.generator.ValueType;
+import prime.tools.generator.output.HaxeOutputUtil;
+import prime.types.SimpleDictionary;
+
+/**
+* Auto generated MassiveUnit Test Class 
+*/
+class AllClassesTest
+{	
+	public function new() 
+	{
+		
+	}
+	
+	@BeforeClass
+	public function beforeClass():Void
+	{
+	}
+	
+	@AfterClass
+	public function afterClass():Void
+	{
+	}
+	
+	@Before
+	public function setup():Void
+	{
+	}
+	
+	@After
+	public function tearDown():Void
+	{
+	}
+	
+	
+	@Test
+	public function testExample():Void
+	{
+		Assert.isTrue(true);
+	}
+}
