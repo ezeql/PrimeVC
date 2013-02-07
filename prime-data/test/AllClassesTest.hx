50b8c6cb8e3a6145ab3aebbc113439784b465161
Andrew Pahuru <andrew@onlinetouch.nl>
2013-02-07 19:18:50 +1300

    Setup mliv and munit for prime-data

---
+++ prime-data/test/AllClassesTest.hx
@@ -0,0 +1,46 @@
+package ;
+
+import massive.munit.util.Timer;
+import massive.munit.Assert;
+import massive.munit.async.AsyncFactory;
+
+
+/**
+* Auto generated MassiveUnit Test Class 
+*/
+class AllClassesTest 
+{
+	
+	
+	public function new() 
+	{
+		
+	}
+	
+	@BeforeClass
+	public function beforeClass():Void
+	{
+	}
+	
+	@AfterClass
+	public function afterClass():Void
+	{
+	}
+	
+	@Before
+	public function setup():Void
+	{
+	}
+	
+	@After
+	public function tearDown():Void
+	{
+	}
+	
+	
+	@Test
+	public function testExample():Void
+	{
+		Assert.isTrue(true);
+	}
+}
