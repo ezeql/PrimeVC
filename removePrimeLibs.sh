0c66f6bf642336bf1f0e2c3b5fb239cc057bb805
Andrew Pahuru <andrew@onlinetouch.nl>
2013-02-11 21:42:04 +1300

    Add empty license files and scripts to install/remove libs locally. Fix up prime-signals for mlib.

---
+++ removePrimeLibs.sh
@@ -0,0 +1,23 @@
+haxelib remove prime-bindable
+
+haxelib remove prime-components
+
+haxelib remove prime-core
+
+haxelib remove prime-css
+
+haxelib remove prime-data
+
+haxelib remove prime-display
+
+haxelib remove prime-fsm
+
+haxelib remove prime-layout
+
+haxelib remove prime-locale
+
+haxelib remove prime-media
+
+haxelib remove prime-mvc
+
+haxelib remove prime-signals
