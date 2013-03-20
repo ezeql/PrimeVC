49a2985524054155253c9c93599d4b05f34c532e
Danny Wilson <danny@onlinetouch.nl>
2013-03-20 23:38:45 +0100

    Start work on prime-perceptor client library. Very rough sketchy code now.

---
+++ prime-perceptor/src/prime/perceptor/AllClasses.hx
@@ -0,0 +1,15 @@
+package prime.perceptor;
+
+import prime.perceptor.Client;
+import prime.perceptor.remoting.Perceptor;
+import prime.perceptor.remoting.Specimen;
+
+@IgnoreCover
+class AllClasses
+{
+@IgnoreCover
+	public static function main():AllClasses {return new AllClasses();}
+@IgnoreCover
+	public function new(){trace('This is a generated main class');}
+}
+
